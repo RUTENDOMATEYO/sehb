1943b2f03b6909573c4242eddcf838731dee1725
brian481 <61460374+brian481@users.noreply.github.com>
2020-02-25 14:25:07 +0200

    Add files via upload

---
+++ h180498Vaverage Heights.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import array as arr
+a = arr.array('i', [112, 104, 126, 128, 121, 111, 109, 122, 133, 200])
+
+n = 10
+
+
+
+print("the average height is ", (a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9])/n)
+
+
